cf2c02b9e95325fa9456fc6bdb8263116c762767
Benedict Jackson <bavjackson@gmail.com>
2025-09-06 16:12:00 +0100

    add extra check for link

--- modules/html-fundamentals/links/tests/links.test.js
+++ modules/html-fundamentals/links/tests/links.test.js
@@ -1,29 +1,39 @@
-import * as fs from 'fs'
-import * as path from 'path'
+import * as fs from "fs";
+import * as path from "path";
 
-import { describe, expect, it, beforeAll } from 'vitest'
-import {JSDOM} from 'jsdom'
+import { describe, expect, it, beforeAll } from "vitest";
+import { JSDOM } from "jsdom";
 
 beforeAll(() => {
-    const fileContents = fs.readFileSync(path.join(__dirname, '../exercise/index.html')).toString()
+  const fileContents = fs
+    .readFileSync(path.join(__dirname, "../exercise/index.html"))
+    .toString();
 
-    global.dom = new JSDOM(fileContents)
-})
-
+  global.dom = new JSDOM(fileContents);
+});
 
 describe("comments", () => {
-    it('list link', () => {
-        const a = global.dom.window.document.querySelector('a[href="./lists.html"]')
+  it("list link", () => {
+    let a = global.dom.window.document.querySelector('a[href="./lists.html"]');
+    if (!a) {
+      a = global.dom.window.document.querySelector('a[href="lists.html"]');
+    }
 
-        expect(a).not.toBeNull()
-        expect(a.textContent).toBeTruthy()
+    expect(a).not.toBeNull();
+    expect(a.textContent).toBeTruthy();
+  });
+  it("contact link", () => {
+    let a = global.dom.window.document.querySelector(
+      'a[href="./pages/contact.html"]',
+    );
+    if (!a) {
+      a = global.dom.window.document.querySelector(
+        'a[href="pages/contact.html"]',
+      );
+    }
 
-    })
-    it('contact link', () => {
-        const a = global.dom.window.document.querySelector('a[href="./pages/contact.html"]')
+    expect(a).not.toBeNull();
+    expect(a.textContent).toBeTruthy();
+  });
+});
 
-        expect(a).not.toBeNull()
-        expect(a.textContent).toBeTruthy()
-
-    })
-})
