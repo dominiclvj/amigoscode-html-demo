141b2e8e80106f7ca8b4905e4cab221e0bb5491d
Benedict Jackson <bavjackson@gmail.com>
2025-09-06 11:17:49 +0100

    fix href test

--- modules/html-fundamentals/attributes_1/tests/attributes_1.test.js
+++ modules/html-fundamentals/attributes_1/tests/attributes_1.test.js
@@ -1,24 +1,25 @@
-import * as fs from 'fs'
-import * as path from 'path'
+import * as fs from "fs";
+import * as path from "path";
 
-import { describe, expect, it, beforeAll } from 'vitest'
-import {JSDOM} from 'jsdom'
+import { describe, expect, it, beforeAll } from "vitest";
+import { JSDOM } from "jsdom";
 
 beforeAll(() => {
-    const fileContents = fs.readFileSync(path.join(__dirname, '../exercise/index.html')).toString()
+  const fileContents = fs
+    .readFileSync(path.join(__dirname, "../exercise/index.html"))
+    .toString();
 
-    global.dom = new JSDOM(fileContents)
-})
-
+  global.dom = new JSDOM(fileContents);
+});
 
 describe("comments", () => {
-    it('link', () => {
-        const link = global.dom.window.document.querySelector('a')
+  it("link", () => {
+    const link = global.dom.window.document.querySelector("a");
 
-        expect(link).not.toBeNull()
-        expect(link.textContent).toBe("Visit Amigoscode")
-        const href = link.getAttribute('href')
-        expect(href).toBe("https://amigoscode.com")
+    expect(link).not.toBeNull();
+    expect(link.textContent).toBe("Visit Amigoscode");
+    const href = link.getAttribute("href");
+    expect(href).toBe("https://www.amigoscode.com");
+  });
+});
 
-    })
-})
